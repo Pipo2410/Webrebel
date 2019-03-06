181e48090fac59777d6cb597ee6c1e8c0140cad8
Peter Strelec <pipo2410@gmail.com>
2019-03-06 16:08:47 +0100

    27 lekcia

--- JavaScript/27/js/script.js
+++ JavaScript/27/js/script.js
@@ -1,19 +1,75 @@
 (function($) {
 
-	var cover = $('#cover'), 
-		covers = $('.fadecovers');
+	// var cover = $('#cover'), 
+	// 	covers = $('.fadecovers');
 
-	covers.children().not(':last').hide();
+	// covers.children().not(':last').hide();
 
-	var coverAnimation = setInterval(function() {
-		covers.children().last()
-			.fadeOut(1500, function() { $(this).prependTo(covers); })
-			.prev().fadeIn(1500);
-	}, 3000);
+	// var sliderInterval = setInterval(function() {
+	// 	covers.children().last()
+	// 		.fadeOut(1500, function() { $(this).prependTo(covers); })
+	// 		.prev().fadeIn(1500);
+	// }, 3000);
 
-	covers.on('click', function() {
-		clearInterval(coverAnimation);
-	});
+	// covers.on('click', function() {
+	// 	clearInterval( sliderInterval );
+	// });
+
 
 })(jQuery);
 
+
+
+var cover = $('#cover'),
+	covers = $('.fadecovers');
+
+covers.children(':not(:last)').hide();
+
+var Slider = {
+
+	intervalID: null,
+	running: false,
+
+	set: function( id ) {
+		this.intervalID = id;
+	},
+
+	get: function() {
+		return 'IDcko intevalu je: ' + this.intervalID;	
+	},
+
+	start: function() {
+		this.intervalID = setInterval(function() {
+			covers.children(':last')
+				.fadeOut(500, function() { $(this).prependTo( covers ) })
+				.prev().fadeIn(500);
+		}, 1000);
+
+		this.running = true;
+		console.log('zacali sme');
+	},
+
+	pause: function() {
+		clearInterval(this.intervalID);
+		this.intervalID = null;
+		
+		this.running = false;
+		console.log('zapauzovali sme');
+	},
+
+	resume: function() {
+		if(!this.intervalID) this.start();
+	},
+
+	toggle: function() {
+		if(this.running) this.pause()
+		else this.resume();
+	}
+
+}
+
+Slider.start();
+
+covers.on('click', function() {
+	Slider.toggle();
+})

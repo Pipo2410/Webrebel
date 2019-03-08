1a05764529eb662deb2b22693d8b5a95655d565d
Peter Strelec <pipo2410@gmail.com>
2019-03-08 09:25:17 +0100

    change layout to flexbox

--- Skuska/js/script.js
+++ Skuska/js/script.js
@@ -1,15 +1,18 @@
 (function($) {
 
-	var discover = $('.btn--animated');
+    var discoverBtn = $('.discover');
 
-	discover.on('click', function(e) {
-		e.preventDefault();
+    discoverBtn.on('click', function(e) {
+        e.preventDefault();
 
-		var id = this.hash;
-		$('html').animate({	scrollTop: $(id).offset().top }, 1500, function() {
-			window.location.hash = id;
-		});
+        var id = this.hash;
 
-	});
+        $('html').animate({
+            scrollTop: $(id).offset().top
+        }, 1500, function() {
+            window.location.hash = id;
+        });
+       
+    });
 
 })(jQuery)

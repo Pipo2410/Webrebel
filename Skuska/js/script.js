65bf5c83a198636235fd0723fcb8c09ba7e9211b
Peter Strelec <pipo2410@gmail.com>
2019-03-11 07:28:42 +0100

    navigation

--- Skuska/js/script.js
+++ Skuska/js/script.js
@@ -15,4 +15,22 @@
        
     });
 
+    var navigaiton = $('.navigation__nav'),
+        navCheck = $('.navigation__checkbox');
+
+    // $('.navigation__button').on('click', function() {
+    //     navCheck.toggle().prop('checked');
+    // })
+
+    navigaiton.find('.navigation__link').on('click', function(e) {
+        e.preventDefault();
+
+        var id = this.hash;
+
+        navCheck.attr('checked', false);
+        $('html').animate({
+            scrollTop: $(this.hash).offset().top,
+        }, 2000);
+    })
+
 })(jQuery)

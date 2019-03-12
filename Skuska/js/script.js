51dd5b3a6a9d0425cbc5ae658f8e72914e0e113b
Peter Strelec <pipo2410@gmail.com>
2019-03-12 10:14:39 +0100

    navigaiton

--- Skuska/js/script.js
+++ Skuska/js/script.js
@@ -1,36 +1,76 @@
+// (function($) {
+
+//     var discoverBtn = $('.discover');
+
+//     discoverBtn.on('click', function(e) {
+//         e.preventDefault();
+
+//         var id = this.hash;
+
+//         $('html').animate({
+//             scrollTop: $(id).offset().top
+//         }, 1500, function() {
+//             window.location.hash = id;
+//         });
+       
+//     });
+
+//     var navigaiton = $('.navigation__nav'),
+//         navCheck = $('.navigation__checkbox');
+
+//     // $('.navigation__button').on('click', function() {
+//     //     navCheck.toggle().prop('checked');
+//     // })
+
+//     navigaiton.find('.navigation__link').on('click', function(e) {
+//         e.preventDefault();
+
+//         var id = this.hash;
+
+//         navCheck.attr('checked', false);
+//         $('html').animate({
+//             scrollTop: $(this.hash).offset().top,
+//         }, 2000);
+//     })
+
+// })(jQuery)
+
 (function($) {
 
-    var discoverBtn = $('.discover');
+    var popupBook = $('#booking'),
+        discoverBtn = $('.discover'),
+        navBtn = $('.navigation__link'),
+        navCheck = $('.navigation__checkbox');
+
+    popupBook.on('click', function(e) {
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 2000);
+    });
 
     discoverBtn.on('click', function(e) {
         e.preventDefault();
 
-        var id = this.hash;
+        var ID = this.hash;
 
-        $('html').animate({
-            scrollTop: $(id).offset().top
-        }, 1500, function() {
-            window.location.hash = id;
+        $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 2000, function() {
+            window.location.hash = ID;
         });
-       
     });
 
-    var navigaiton = $('.navigation__nav'),
-        navCheck = $('.navigation__checkbox');
-
-    // $('.navigation__button').on('click', function() {
-    //     navCheck.toggle().prop('checked');
-    // })
-
-    navigaiton.find('.navigation__link').on('click', function(e) {
+    
+    navBtn.on('click', function(e) {
         e.preventDefault();
 
-        var id = this.hash;
+        var ID = this.hash
 
-        navCheck.attr('checked', false);
-        $('html').animate({
-            scrollTop: $(this.hash).offset().top,
-        }, 2000);
-    })
+        navCheck.prop('checked', false);
+        $('html, body').animate({ scrollTop: $(ID).offset().top }, 1500, function() {
+            window.location.hash = ID;
+        });
+    });
+
 
 })(jQuery)

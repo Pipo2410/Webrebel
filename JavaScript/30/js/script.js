8bdc8c370fd93ed333bd9b091419f0b86356d69d
Peet <pipo2410@gmail.com>
2019-03-14 16:18:54 +0100

    30

--- JavaScript/30/js/script.js
+++ JavaScript/30/js/script.js
@@ -7,7 +7,7 @@
 // 	})
 // 	.animate({ top: -800 }, 800);
 
-// $('.post-title')
+// $('.post-title') 
 // 	.css({ position: 'relative' })
 // 	.on('click', function() {
 // 		$(this).animate({ left: '+=50' });
@@ -26,3 +26,21 @@
 // 	});
 
 
+
+
+(function($){
+
+	$.fx.speeds.slowAsShit = 5432;
+
+	var colors = [ '#3b9ae1', '#f6be00', '#e64134', '#eb70b1', '#0f1a5f'];
+
+	$('.nav-bar').find('li').on('mouseenter', function() {
+		
+		if ( $(this).is(':animated') ) return;
+		
+		var newColor = colors[Math.floor(Math.random() * colors.length)];
+		$(this)
+			.animate({ backgroundColor: newColor }, 'slowAsShit');
+	});
+
+})(jQuery)

cc8d7cd5adb5d51bfc9db10faccaefc07c9c2994
Peet <pipo2410@gmail.com>
2019-08-08 20:07:45 +0200

    lecture 40

--- JavaScript/E40/js/app.js
+++ JavaScript/E40/js/app.js
@@ -2,7 +2,24 @@
 var progress = document.querySelector('.progress'),
 	textarea = document.querySelector('textarea');
 
-var pathLength = progress.getTotalLength();
+var pathLength = progress.getTotalLength(),
+	tweetLength = 140;
 
 progress.style.strokeDasharray = pathLength + 'px';
 progress.style.strokeDashoffset = pathLength + 'px';
+
+
+// on input
+textarea.addEventListener('input', function(event) {
+
+	var len = textarea.value.length,
+		per = len / tweetLength,
+		newOffset = pathLength - (pathLength * per) + 'px';
+
+	console.log(len, per);
+	console.log(newOffset);
+	console.log('');
+
+	
+	progress.style.strokeDashoffset = newOffset;
+})
